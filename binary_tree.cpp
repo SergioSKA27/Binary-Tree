09974414e3ae898f6064045098482be28f15f271
SergioSka27 <sergioska81@hotmail.com>
2020-12-22 03:45:38 -0600

    Nodes implemented

--- binary_tree.cpp
+++ binary_tree.cpp
@@ -13,7 +13,7 @@ public:
     Node<type> *rigth;
 
     Node(type Data);
-    Node(type Data);
+    Node();
     Node<type> &changedata(type data);
     type getdata();
     void showdata();
@@ -21,10 +21,56 @@ public:
     ~Node();
 };
 
-Node::Node(/* args */)
+template <class type>
+Node<type>::Node(type Data)
+{
+    this->Data = Data;
+    this->left = NULL;
+    this->rigth = NULL;
+}
+
+template <class type>
+Node<type>::Node()
+{
+    this->left = NULL;
+    this->rigth = NULL;
+}
+
+template <class type>
+Node<type> &Node<type>::changedata(type newdata)
+{
+    Data = newdata;
+    return *this;
+}
+
+template <class type>
+type Node<type>::getdata()
+{
+    return this->Data;
+}
+
+template <class type>
+void Node<type>::showdata()
+{
+    std::cout << this->Data << std::endl;
+}
+
+template <class type>
+Node<type> &Node<type>::operator=(const Node<type> &node2)
+{
+    this->Data = node2.Data;
+    this->left = node2.left;
+    this->rigth = node2.rigth;
+    return *this;
+}
+
+template <class type>
+Node<type>::~Node()
 {
 }
 
-Node::~Node()
+int main(int argc, char const *argv[])
 {
+
+    return 0;
 }
