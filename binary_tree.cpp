57e8550c5946c8ba20e57ceefda3c670f621335f
SergioSka27 <sergioska81@hotmail.com>
2021-01-17 02:48:07 -0600

    Nuevas funciones añadidas

--- binary_tree.cpp
+++ binary_tree.cpp
@@ -79,7 +79,7 @@ template <class Type>
 class Binary_tree
 {
 private:
-    Node<Type> Root;   //Raiz del arbol
+    Node<Type> *Root;  //Raiz del arbol
     Node<Type> *nodes; //Variable para crear los nodos
     int nods;          //Numero de nodos del arbol
     int levels;        //Niveles del arbol
@@ -110,14 +110,18 @@ public:
     bool complete(); //Verificar si esta completo
 
     //Datos del arbol
-    int countnodes();  //retorna el numero de nodos del arbol
-    int countlevels(); //retorna el numero de niveles de el arbol
-    int countleafs();  //retorna el numero de hojas del arbol
+    int countnodes();                //retorna el numero de nodos del arbol
+    int countlevels();               //retorna el numero de niveles de el arbol
+    int countleafs(Node<Type> *nde); //retorna el numero de hojas del arbol o sub-arbol
+    int countleafs();                //retorna el numero de hojas del arbol o sub-arbol
 
     //Funciones de impresion
     void showroot();
     void printtree();                        //Imprimir el arbol completo
     void print_subtree(Node<Type> *Subtree); // seleccionar un nodo e imprimir su arbol de decendientes
+
+    //Funciones de borrado
+    void prune(Node<Type> *nde); //Elimina el nodo y todo su arbol de descendientes
 
     ~Binary_tree();
 };
@@ -125,7 +129,7 @@ public:
 template <class Type>
 Binary_tree<Type>::Binary_tree(Type Data)
 {
-    this->Root.changedata(Data);
+    this->Root = new Node<Type>(Data);
     this->nodes = NULL;
     this->leafs = 0;
     this->levels = 1;
@@ -136,6 +140,7 @@ Binary_tree<Type>::Binary_tree(Type Data)
 template <class Type>
 Binary_tree<Type>::Binary_tree()
 {
+    this->Root = NULL;
     this->nodes = NULL;
     this->leafs = 0;
     this->levels = 0;
@@ -146,9 +151,10 @@ Binary_tree<Type>::Binary_tree()
 template <class Type>
 Binary_tree<Type> &Binary_tree<Type>::setroot(Type Data)
 {
-    Root.changedata(Data);
+    if (this->Root == NULL)
+        this->Root = new Node<Type>(Data);
+    Root->changedata(Data);
     emptystate = false;
-    nods = 0;
     return *this;
 }
 
@@ -190,8 +196,8 @@ Node<Type> *Binary_tree<Type>::insertleft(Type Data)
 
     this->nodes = new Node<Type>();
     this->nodes->changedata(Data);
-    this->Root.left = this->nodes;
-    this->nodes->father = &this->Root;
+    this->Root->left = this->nodes;
+    this->nodes->father = this->Root;
     this->nods += 1;
 
     return this->nodes;
@@ -205,8 +211,8 @@ Node<Type> *Binary_tree<Type>::insertrigth(Type Data)
 
     this->nodes = new Node<Type>();
     this->nodes->changedata(Data);
-    this->Root.rigth = this->nodes;
-    this->nodes->father = &this->Root;
+    this->Root->rigth = this->nodes;
+    this->nodes->father = this->Root;
     this->nods += 1;
 
     return this->nodes;
@@ -215,7 +221,7 @@ Node<Type> *Binary_tree<Type>::insertrigth(Type Data)
 template <class Type>
 Node<Type> *Binary_tree<Type>::root()
 {
-    return &this->Root;
+    return this->Root;
 }
 
 template <class Type>
@@ -265,6 +271,13 @@ void Binary_tree<Type>::pre_order(Node<Type> *nde, std::vector<Type> &list)
         this->in_order(nde->rigth, list);
     }
 }
+
+template <class Type>
+bool Binary_tree<Type>::empty()
+{
+    return this->emptystate;
+}
+
 /*
 template <class Type>
 Binary_tree<Type>::Binary_tree(Type Data)
@@ -278,27 +291,59 @@ template <class Type>
 Binary_tree<Type>::Binary_tree(Type Data)
 {
 }
+*/
+
 template <class Type>
-Binary_tree<Type>::Binary_tree(Type Data)
+int Binary_tree<Type>::countleafs(Node<Type> *nde)
 {
+    if (nde == NULL)
+    {
+        return 0;
+    }
+    else
+    {
+        int A = this->countleafs(nde->left);
+        int B = this->countleafs(nde->rigth);
+
+        if (A == 0 && B == 0)
+        {
+            this->leafs += 1;
+        }
+        return this->leafs;
+    }
 }
+
 template <class Type>
-Binary_tree<Type>::Binary_tree(Type Data)
+int Binary_tree<Type>::countleafs()
 {
+    return this->countleafs(this->Root);
 }
-template <class Type>
-Binary_tree<Type>::Binary_tree(Type Data)
-{
-}*/
+
 template <class Type>
 void Binary_tree<Type>::showroot()
 {
-    std::cout << this->Root.getdata() << std::endl;
+    std::cout << this->Root->getdata() << std::endl;
+}
+
+template <class Type>
+void Binary_tree<Type>::prune(Node<Type> *nde)
+{
+    Node<Type> *tmp = NULL;
+    if (nde == NULL)
+        return;
+    else
+    {
+        tmp = nde;
+        prune(nde->left);
+        prune(nde->rigth);
+        delete tmp;
+    }
 }
 
 template <class Type>
 Binary_tree<Type>::~Binary_tree()
 {
+    this->prune(this->Root);
 }
 
 template <class t>
@@ -312,18 +357,147 @@ void printvector(std::vector<t> &v)
     std::cout << std::endl;
 }
 
+//funciones para crear arboles de expresiones
+bool is_operator(std::string op)
+{
+    return (op == "+" || op == "-" || op == "*" || op == "/" || op == "^");
+}
+
+void insert_hijos(Binary_tree<std::string> &tree, Node<std::string> *node, std::stack<std::string> &Stack)
+{
+    if (Stack.empty()) //si la pila esta vacia retornamos
+        return;
+
+    if (is_operator(Stack.top()) && tree.empty()) //si el arbol esta vacio colocamos el top como raiz
+    {
+        tree.setroot(Stack.top());
+        Stack.pop();
+        Node<std::string> *r = tree.root(); //puntero a la raiz
+
+        insert_hijos(tree, r, Stack); //insertamos los hijos a la derecha de la raiz
+
+        if (is_operator(Stack.top()))
+        { //insertamos los hijos a la izquierda de la raiz
+            Node<std::string> *izq = tree.insertleft(r, Stack.top());
+            Stack.pop();
+            insert_hijos(tree, izq, Stack);
+            return;
+        }
+        else
+        {
+            tree.insertleft(Stack.top());
+            Stack.pop();
+            return;
+        }
+    }
+
+    if (is_operator(Stack.top()))
+    {
+        Node<std::string> *aux = tree.insertrigth(node, Stack.top());
+        Stack.pop();
+        insert_hijos(tree, aux, Stack);
+
+        if (is_operator(Stack.top()))
+        {
+            Node<std::string> *aux2 = tree.insertleft(node, Stack.top());
+            Stack.pop();
+            insert_hijos(tree, aux2, Stack);
+            return;
+        }
+        else
+        {
+            tree.insertleft(node, Stack.top());
+            Stack.pop();
+            return;
+        }
+    }
+    else
+    {
+        tree.insertrigth(node, Stack.top());
+        Stack.pop();
+        return;
+    }
+}
+
+/*void insert_hijos(Node<std::string> *node, std::stack<Node<std::string> *> &Stack)
+{ //inserta los hijos de un nodo tomando un stack de nodos y insertando los hijos de manera recursiva
+    if (Stack.empty())
+        return;
+    if (is_operator(Stack.top()->getdata()))
+    {
+        node->rigth = Stack.top();
+        Stack.pop();
+        insert_hijos(node->rigth, Stack);
+
+        if (is_operator(Stack.top()->getdata()))
+        {
+            node->left = Stack.top();
+            Stack.pop();
+            insert_hijos(node->left, Stack);
+        }
+        else
+        {
+            node->left = Stack.top();
+            Stack.pop();
+        }
+    }
+    else
+    {
+        node->rigth = Stack.top();
+        Stack.pop();
+
+        if (is_operator(Stack.top()->getdata()))
+        {
+            node->left = Stack.top();
+            Stack.pop();
+            insert_hijos(node->left, Stack);
+        }
+        else
+        {
+            node->left = Stack.top();
+            Stack.pop();
+        }
+    }
+}
+*/
+void sufijo_arbol(std::string exp)
+{
+    std::stack<std::string> Str;
+    Binary_tree<std::string> arbol;
+    std::vector<std::string> V;
+
+    for (int i = 0; i < exp.size(); i++)
+    {
+        std::string actual(1, exp[i]);
+
+        Str.push(actual);
+    }
+
+    insert_hijos(arbol, NULL, Str);
+
+    arbol.pre_order(arbol.root(), V);
+
+    printvector<std::string>(V);
+}
+
 int main(int argc, char const *argv[])
 {
-    Binary_tree<int> BIN(10);
-    std::vector<int> InO;
-    BIN.setroot(100);
-    BIN.showroot();
-    BIN.insertleft(BIN.insertleft(BIN.insertleft(1), 10), 15);
-    BIN.insertrigth(BIN.insertrigth(BIN.insertrigth(BIN.root(), -4), 10), -111);
+    std::string s = "72ˆ653+*+9-";
 
-    BIN.root()->left->left->left->showdata();
-    BIN.pre_order(BIN.root(), InO);
+    sufijo_arbol(s);
+
+    /*Binary_tree<int> BIN(10);
+    std::vector<int> InO;
+    BIN.setroot(1);
+    BIN.showroot();
+    BIN.insertleft(BIN.insertleft(2), 4);
+    BIN.insertrigth(BIN.root()->left, 5);
+    BIN.insertrigth(BIN.insertrigth(BIN.insertrigth(3), 6), 9);
+    BIN.insertleft(BIN.insertleft(BIN.root()->rigth->rigth, 8), 11);
+
+    BIN.post_order(BIN.root(), InO);
     printvector(InO);
+    std::cout << BIN.countleafs() << std::endl;*/
 
     return 0;
 }
